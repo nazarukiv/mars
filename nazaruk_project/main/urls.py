7c1efa1b5dfff081d3f44badc339a899ab7e35cc
Ivan Nazaruk <nazaruk7649@ukr.net>
2023-10-05 18:44:05 +0100

    eve study

--- nazaruk_project/main/urls.py
+++ nazaruk_project/main/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index),
-    path('about', views.about ),
-    path('ivan', views.ivan)
-]
+    path('about', views.about, name='about'),
+    path('ivan', views.ivan),
+    path('index', views.index, name='home'),
+    path('contact_us', views.contact, name='contact')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- nazaruk_project/main/views.py
+++ nazaruk_project/main/views.py
@@ -3,10 +3,22 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        "title": "About This Project",
+        'values' : ['Example', 'OF', 'Values'],
+        'obj' : {
+            'car' : 'Mercedes',
+            'age' : 17,
+            'hobby' : 'gym and boxing'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
 
 def ivan(request):
     return HttpResponse("<h4>Checking of work.My name is Ivan Nazaruk</h4>")
