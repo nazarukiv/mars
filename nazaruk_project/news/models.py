cd6ce1157ad4cae94804e629df0ae951fda1a92d
Ivan Nazaruk <nazaruk7649@ukr.net>
2023-10-10 19:00:58 +0100

    10.10.2023

--- nazaruk_project/news/models.py
+++ nazaruk_project/news/models.py
@@ -10,6 +10,10 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return f'/news/{self.id}'
+
     class Meta:
         verbose_name = "News"
         verbose_name_plural = 'News'
+
--- nazaruk_project/news/views.py
+++ nazaruk_project/news/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 from .forms import ArticlesForm
+from django.views.generic import DetailView, ListView, UpdateView, DeleteView
+
 
 def news_home(request):
     news = Article.objects.all().order_by("-date")[:]
     return render(request, 'news/news_home.html', {'news' : news})
+
+
+class NewsDeleteView(DeleteView):
+    model = Article
+    success_url = '/news/'
+    template_name = 'news/news-delete.html'
+
+class NewsUpdateView(UpdateView):
+    model = Article
+    template_name = 'news/create.html'
+
+    form_class = ArticlesForm
+
+class NewsDetailView(DetailView):
+    model = Article
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
 
 def create(request):
     error = ''
@@ -12,6 +32,7 @@ def create(request):
         form = ArticlesForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('home')
         else:
             error = 'Error(form was incorrect)'
 
